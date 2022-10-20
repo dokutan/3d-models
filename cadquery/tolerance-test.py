285b6f6c15a5d48715316b08be072e736794be7b
dokutan <54861821+dokutan@users.noreply.github.com>
2022-10-20 13:42:27 +0200

    Update tolerance-test.py

--- cadquery/tolerance-test.py
+++ cadquery/tolerance-test.py
@@ -11,12 +11,6 @@ def test_pin(diameter):
     result = cq.Workplane("XY").circle(diameter / 2).extrude(21).faces("<Z").circle(3).extrude(2)
     return result
 
-def test_moving(diameter, label):
-    outer = cq.Workplane("XY").box(diameter + 4, diameter + 4, 5)
-    outer = outer.faces(">X").workplane().center(-0.1, -0).text(label, 5, -0.4, font="DejaVu Sans")
-    inner = cq.Workplane("XY").circle(diameter / 2).extrude(10, both=True)
-    return outer - inner
-
 boxes = \
     test_box(3.0, ".0").translate((0, 0,  0)) | \
     test_box(3.1, ".1").translate((0, 15, 0)) | \
